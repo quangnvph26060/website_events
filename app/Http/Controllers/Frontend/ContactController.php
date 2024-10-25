b8b92d4a861f84550969b1a461f518915b4035e3
quangan010903@gmail.com <quangan010903@gmail.com>
2024-10-25 15:22:03 +0700

    fix submit contact form

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
@@ -15,22 +16,26 @@ class ContactController extends Controller
     }
     public function contactSubmit(Request $request)
     {
-        $criteria = $request->validate([
-            'fullName' => 'required|string|max:255',
+        $validator = Validator::make($request->all(), [
+            'fullName' => 'required|string|min:3|max:255',
             'email' => 'required|email',
             'phone' => ['required', 'regex:/^(03|05|07|08|09)[0-9]{8}$/'],
-            'company' => 'required|string|max:255',
-            'subject' => 'required|string|max:255',
+            'company' => 'required|string|min:3|max:255',
+            'subject' => 'required|string|min:3|max:255',
             'message' => 'required'
         ], __('request.messages'), [
             'fullName' => 'Họ và tên',
+            'email' => 'Email',
             'phone' => 'Số điện thoại',
             'company' => 'Công ty',
             'subject' => 'Tiêu đề',
             'message' => 'Tin nhắn'
         ]);
-        Contact::create($criteria);
-        session()->flash('success', 'Đã gửi tin nhắn thành công');
-        return redirect()->back();
+        if ($validator->fails()) {
+
+            return response()->json(['error' => $validator->errors()]);
+        }
+        Contact::create($request->all());
+        return response()->json(['success' => 'Đã gửi tin nhắn liên hệ thành công']);
     }
 }
