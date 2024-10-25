0221c3ccd1059e6fd671d05601696dff3dc45213
quangan010903@gmail.com <quangan010903@gmail.com>
2024-10-25 22:02:12 +0700

    show about us

--- app/Http/Controllers/Frontend/AboutController.php
+++ app/Http/Controllers/Frontend/AboutController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\About;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
     function about(){
-        return view('frontend.pages.about');
+        $aboutUs = About::all();
+        return view('frontend.pages.about' , compact('aboutUs'));
     }
 }
