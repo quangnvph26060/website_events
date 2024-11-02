de655aa4f963c7eec2c2b8173c7610beac57fac8
datdev <dnguyentien145@gmail.com>
2024-11-02 13:32:29 +0700

    update code

--- app/Http/Controllers/Backend/ConfigController.php
+++ app/Http/Controllers/Backend/ConfigController.php
@@ -20,34 +20,37 @@ class ConfigController extends Controller
 
     public function update(Request $request)
     {
+        $validated =  $request->validate(
+            [
+                'title_seo' => 'required|string|max:255',
+                'meta_seo' => 'nullable|string|max:255',
+                'description_seo' => 'nullable|string|max:300',
+                'description' => 'nullable|string|max:255',
+                'address' => 'nullable|string|max:255',
+                'email' => 'nullable|string|max:255',
+                'constant_hotline' => 'nullable|string|max:255',
+                'footer' => 'nullable|string|max:255',
+                'icon' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'head_scripts' => 'nullable|string',
+                'body_scripts' => 'nullable|string',
+                'footer_scripts' => 'nullable|string',
+            ],
+            __('request.messages')
+        );
 
-        $validated =  $request->validate([
-            'title_seo' => 'required|string|max:255',
-            'meta_seo' => 'nullable|string|max:255',
-            'description_seo' => 'nullable|string|max:300',
-            'description' => 'nullable|string|max:255',
-            'address' => 'nullable|string|max:255',
-            'email' => 'nullable|string|max:255',
-            'constant_hotline' => 'nullable|string|max:255',
-            'footer' => 'nullable|string|max:255',
-            'icon' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'head_scripts' => 'nullable|string',
-            'body_scripts' => 'nullable|string',
-            'footer_scripts' => 'nullable|string',
-        ],   __('request.messages'));
 
         $config = Config::first();
 
 
         if ($request->hasFile('icon')) {
             deleteImage($config->icon);
-            $validated['icon'] = saveImages($request, 'icon', 'icon', );
+            $validated['icon'] = saveImages($request, 'icon', 'icon',);
         }
 
         if ($request->hasFile('logo')) {
             deleteImage($config->logo);
-            $validated['logo'] = saveImages($request, 'logo', 'logo', 2732,1200);
+            $validated['logo'] = saveImages($request, 'logo', 'logo', 2732, 1200);
         }
 
         if ($config) {
